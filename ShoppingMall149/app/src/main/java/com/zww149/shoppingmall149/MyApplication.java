5fd08a17573bc97c09e5704e8c85861762dfe2e9
“xianyu0905 <1103048750@qq.com>
2019-11-06 01:07:20 +0800

    13OkhttpUtils

---
+++ ShoppingMall149/app/src/main/java/com/zww149/shoppingmall149/MyApplication.java
@@ -0,0 +1,35 @@
+package com.zww149.shoppingmall149;
+
+import android.app.Application;
+
+
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * 作用：整个软件
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        /**
+         * 初始化okhttpUtils
+         */
+        initOkhttpClient();
+    }
+
+    private void initOkhttpClient() {
+      OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .addInterceptor(new LoggerInterceptor("TAG"))
+                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
+                .readTimeout(10000L, TimeUnit.MILLISECONDS)
+                //其他配置
+                .build();
+
+        OkHttpUtils.initClient(okHttpClient);
+    }
+}
---
+++ ShoppingMall149/app/src/main/java/com/zww149/shoppingmall149/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.zww149.shoppingmall149.utils;
+
+/**
+ * 作用：配置各个页面联网地址
+ */
+public class Constants {
+    public static String HOME_URL = "http://192.168.43.200:8080/atguigu/json/HOME_URL.json";
+}
