75ebfe08cb3c18e315b4be60e08b014c88131516
“xianyu0905 <1103048750@qq.com>
2019-11-06 01:00:35 +0800

    使用OkHttpUtils请求网络

--- ShoppingMall149/app/src/main/java/com/zww149/shoppingmall149/home/fragment/HomeFragment.java
+++ ShoppingMall149/app/src/main/java/com/zww149/shoppingmall149/home/fragment/HomeFragment.java
@@ -1,33 +1,112 @@
 package com.zww149.shoppingmall149.home.fragment;
 
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+import com.zww149.shoppingmall149.R;
 import com.zww149.shoppingmall149.base.BaseFragment;
+import com.zww149.shoppingmall149.utils.Constants;
 
+import okhttp3.Call;
 
 
 /**
  * 作用：主页面Fragment
  */
 public class HomeFragment extends BaseFragment {
-    private static final String TAG = HomeFragment.class.getSimpleName();
-    private TextView textView;
+    private static final String TAG =
+            HomeFragment.class.getSimpleName();
+    private RecyclerView rvHome;
+    private ImageView ib_top;
+    private TextView tv_search_home;
+    private TextView tv_message_home;
+
+    @Override
+    public View initView() {
+        Log.e(TAG, "主页视图被初始化了");
+        View view = View.inflate(mContext, R.layout.fragment_home,
+                null);
+        rvHome = view.findViewById(R.id.rv_home);
+        ib_top = view.findViewById(R.id.ib_top);
+        tv_search_home = view.findViewById(R.id.tv_search_home);
+        tv_message_home = view.findViewById(R.id.tv_message_home);
+        //设置点击事件
+        initListener();
+        return view;
+    }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("主页");
+        Log.e(TAG, "主页数据被初始化了");
+        //联网请求主页的数据
+        getDataFromNet();
+     }
+
+    private void getDataFromNet() {
+       // String url = Constants.HOME_URL;
+        OkHttpUtils
+                .get()
+                .url(Constants.HOME_URL)
+                /* .addParams("username", "hyman")
+                 .addParams("password", "123")*/
+                .id(100)
+                .build()
+                .execute(new StringCallback() {
+                    /**
+                     * 当请求失败的时候
+                     * @param call
+                     * @param e
+                     * @param id
+                     */
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e(TAG, "首页请求失败==" + e.getMessage());
+                    }
+
+                    /**
+                     * 当联网成功的时候回调
+                     * @param response 请求成功的数据
+                     * @param id
+                     */
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e(TAG, "首页请求成功==" + response);
+                    }
+                });
     }
 
-    @Override
-    public View initView() {
-        Log.e(TAG,"主页面的Fragment的UI被初始化了");
-        textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        return textView;
+    private void initListener() {
+        //置顶的监听
+        ib_top.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //回到顶部
+                rvHome.scrollToPosition(0);
+            }
+        });
+        //搜素的监听
+        tv_search_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "搜索",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        //消息的监听
+        tv_message_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "进入消息中心",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
